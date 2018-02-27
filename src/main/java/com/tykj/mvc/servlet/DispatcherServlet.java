de092b791f12d3f63b8a0046a98cbe36488de863
13807615840 <13807615840@163.com>
2018-02-27 22:22:08 +0800

    mvc 导入

--- src/main/java/com/tykj/mvc/servlet/DispatcherServlet.java
+++ src/main/java/com/tykj/mvc/servlet/DispatcherServlet.java
@@ -54,10 +54,10 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
 
-        super.init(config);
         initPackage(basePackage);
         initInstance();
         initAutowired();
+        initMappingInfo();
     }
 
     void initInstance() {
